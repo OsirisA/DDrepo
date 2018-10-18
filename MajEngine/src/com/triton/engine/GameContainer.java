8b3202f8cedf49115674aef40fe487b381a98f31
OsirisA <christos_moisiadis@hotmail.com>
2018-10-18 21:59:14 +0200

    * Window added , Some minor changes to GameContainer

--- MajEngine/src/com/triton/engine/GameContainer.java
+++ MajEngine/src/com/triton/engine/GameContainer.java
@@ -3,9 +3,13 @@ package com.triton.engine;
 public class GameContainer implements Runnable {
 
 	private Thread thread;
-
+	private Window window;
+	
 	private boolean running = false;
 	private final double UPDATE_CAP = 1.0 / 60.0;
+	private int width =320 , height =240;
+	private float scale = 4f;
+	private String title = "My first Game v1.0";
 
 	public GameContainer() {
 
@@ -13,6 +17,8 @@ public class GameContainer implements Runnable {
 
 	public void start() {
 
+		window = new Window(this);
+		
 		thread = new Thread(this);
 		thread.run();
 	}
@@ -57,7 +63,7 @@ public class GameContainer implements Runnable {
 				}
 
 				if (render) {
-					// TODO
+					window.update();
 					frames++;
 				} else {
 					try {
@@ -79,5 +85,45 @@ public class GameContainer implements Runnable {
 	public static void main(String args[]) {
 		GameContainer gc = new GameContainer();
 		gc.start();
+	}
+
+	public boolean isRunning() {
+		return running;
+	}
+
+	public void setRunning(boolean running) {
+		this.running = running;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public float getScale() {
+		return scale;
+	}
+
+	public void setScale(float scale) {
+		this.scale = scale;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
 	}
 }
